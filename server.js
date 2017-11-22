e1f00ee242a5224b1cc6712bfe94021217705084
KOTSEV Alexander <alexander.kotsev@ec.europa.eu>
2017-11-22 08:42:18 -0500

    Added source for API

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 8000,
+  mongoose = require('mongoose'),
+  Task = require('./models/Model'), //created model loading here
+  bodyParser = require('body-parser');
+  
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Tododb',
+{
+useMongoClient: true,
+}
+); 
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var routes = require('./routes/Routes'); //importing route
+routes(app); //register the route
+
+app.listen(port);
+
+app.use(function(req, res) {
+  res.status(404).send({url: req.originalUrl + ' not found'})
+});
+
+console.log('todo list RESTful API server started on: ' + port);
